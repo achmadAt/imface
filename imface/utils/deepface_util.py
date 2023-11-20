1145f2cb2c763e505434daecfdbfc46a39108be3
achmad <achmadalfazari@gmail.com>
2023-11-20 18:06:39 +0700

    fix: change to dlib

--- imface/utils/deepface_util.py
+++ imface/utils/deepface_util.py
@@ -17,19 +17,20 @@ models = [
 
 backends = [
     "retinaface",
+    "dlib"
 ]
 
 
 def getEmbeddingVector(path: str):
     embed = []
-    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[0])
+    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[1])
     for imgdata in data:
         embed.append(imgdata['embedding'])
     
     return embed
 
 def extractFace(path: str):
-    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[0])
+    data = DeepFace.represent(path, model_name=models[2], enforce_detection=True, detector_backend=backends[1])
     return data
 
 def getDistance(source, target):
